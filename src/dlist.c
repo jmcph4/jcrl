908a6f0fb2bd08a2e5f533686d1bf52e237886c2
Jack McPherson <jmcph4@users.noreply.github.com>
2016-11-24 09:36:34 +1000

    Add list equal, append, change concatenation, ...
    This commit adds equality checking to the doubly-linked list
    implementation. This is implemented in `dlist_equal`
    (in `src/dlist.c`).
    
    This commit also adds append to doubly-linked lists, via `dlist_append`
    (in `src/dlist.c`).
    
    This commit also changes the behaviour of doubly-linked list
    concatenation. Now, only two doubly-linked lists are specified,
    with the second being appended onto the first (i.e. only the first is
    modified).
    
    This commit also reformats the doubly-linked list implementation to
    better align with recent style guidelines.

--- src/dlist.c
+++ src/dlist.c
@@ -1,333 +1,362 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "constants.h"
 #include "dlist.h"
 
 unsigned int dlist_init(DList* list)
 {
-  if(list == NULL)
-  {
-    return JCRL_ERR_NULL_PARAM;
-  }
+    if(list == NULL)
+    {
+        return JCRL_ERR_NULL_PARAM;
+    }
   
-  list->length = 0;
-  list->head = NULL;
-  list->tail = NULL;
+    list->length = 0;
+    list->head = NULL;
+    list->tail = NULL;
 
-  return JCRL_ERR_OK;
+    return JCRL_ERR_OK;
 }
 
 unsigned int dlist_free(DList* list)
 {
-  struct _DNode* ptr = list->head;
-  struct _DNode* prev = NULL;
+    struct _DNode* ptr = list->head;
+    struct _DNode* prev = NULL;
   
-  for(ptr=list->head;ptr!=NULL;ptr=ptr->next)
-  {
-    if(ptr != list->head)
+    for(ptr=list->head;ptr!=NULL;ptr=ptr->next)
     {
-      free(prev);
-      prev = ptr;
+        if(ptr != list->head)
+        {
+        free(prev);
+        prev = ptr;
+        }
     }
-  }
-  
-  free(list->head);
-  list->length = 0;
-
-  return JCRL_ERR_OK;
+    
+    free(list->head);
+    list->length = 0;
+    
+    return JCRL_ERR_OK;
 }
 
 unsigned int dlist_insert(void* data, unsigned int pos, DList* list)
 {
-  // initial checks
-  if(list == NULL)
-  {
-    return JCRL_ERR_NULL_PARAM;
-  }
-  
-  if(pos > list->length)
-  {
-    return JCRL_ERR_OUT_OF_BOUNDS;
-  }
-  
-  // allocate new node
-  struct _DNode* new_node = calloc(1, sizeof(struct _DNode));
-  
-  if(new_node == NULL)
-  {
-    return JCRL_ERR_SYS_MEM_ALLOC;
-  }
-  
-  // initialise new node
-  new_node->data = data;
-  new_node->next = NULL;
-  new_node->prev = NULL;
-  
-  unsigned int i = 0;
-  struct _DNode* ptr = list->head;
-  
-  if(list->length == 0)
-  {
-    list->head = new_node;
-    list->tail = new_node;
-    
-    list->length++;
-    
-    return JCRL_ERR_OK;
-  }
-
-  if(pos == 0) // inserting new head
-  {
-    new_node->next = list->head;
-	
-    if(list->head->next != NULL)
+    /* initial checks */
+    if(list == NULL)
     {
-      list->head->next->prev = new_node;
+        return JCRL_ERR_NULL_PARAM;
     }
     
-    list->head = new_node;
-    
-    list->length++;
-        
-    return JCRL_ERR_OK;
-  }
-    
-  for(ptr=list->head;ptr!=NULL;ptr=ptr->next)
-  {
-    if(i + 1 == pos)
+    if(pos > list->length)
     {
-      new_node->next = ptr->next;
-      new_node->prev = ptr;
+        return JCRL_ERR_OUT_OF_BOUNDS;
+    }
     
-      if(ptr->next != NULL)
-      {
-        ptr->next->prev = new_node;
-      }
-      else
-      {
+    struct _DNode* new_node = calloc(1, sizeof(struct _DNode));
+    
+    if(new_node == NULL)
+    {
+        return JCRL_ERR_SYS_MEM_ALLOC;
+    }
+    
+    // initialise new node
+    new_node->data = data;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+    
+    unsigned int i = 0;
+    struct _DNode* ptr = list->head;
+    
+    if(list->length == 0)
+    {
+        list->head = new_node;
         list->tail = new_node;
-      }
-      
-      ptr->next = new_node;
-      
-      break;
+        
+        list->length++;
+        
+        return JCRL_ERR_OK;
     }
-  
-    i++;
-  }
-  
-  list->length++;
-
-  return JCRL_ERR_OK;
+    
+    if(pos == 0) // inserting new head
+    {
+        new_node->next = list->head;
+        
+        if(list->head->next != NULL)
+        {
+            list->head->next->prev = new_node;
+        }
+        
+        list->head = new_node;
+        
+        list->length++;
+            
+        return JCRL_ERR_OK;
+    }
+        
+    for(ptr=list->head;ptr!=NULL;ptr=ptr->next)
+    {
+        if(i + 1 == pos)
+        {
+            new_node->next = ptr->next;
+            new_node->prev = ptr;
+        
+            if(ptr->next != NULL)
+            {
+                ptr->next->prev = new_node;
+            }
+            else
+            {
+                list->tail = new_node;
+            }
+        
+            ptr->next = new_node;
+        
+            break;
+        }
+    
+        i++;
+    }
+    
+    list->length++;
+    
+    return JCRL_ERR_OK;
 }
 
 unsigned int dlist_remove(unsigned int pos, DList* list)
 {
-  if(list == NULL)
-  {
-    return JCRL_ERR_NULL_PARAM;
-  }
-  
-  if(pos > list->length)
-  {
-    return JCRL_ERR_OUT_OF_BOUNDS;
-  }
-  
-  unsigned int i = 0;
-  struct _DNode* tmp = NULL;
-  struct _DNode* ptr = list->head;
-  
-  if(pos == 0) // removing head
-  {
-    tmp = list->head;
-	
-	if(list->head->next != NULL)
-	{
-	  list->head->next->prev = NULL;
-	}
-	
-    list->head = list->head->next;
-  }
-  else
-  {
-    for(ptr=list->head;ptr!=NULL;ptr=ptr->next)
+    if(list == NULL)
     {
-      if(i + 1 == pos)
-      {
-        tmp = ptr->next;
-        
-		if(ptr->next->next != NULL)
-		{
-		  ptr->next->next->prev = ptr;
-		}
-		else
-		{
-		  list->tail = ptr;
-		}
-		
-		ptr->next = ptr->next->next;
-		
-		break;
-      }
-    
-      i++;
+        return JCRL_ERR_NULL_PARAM;
     }
-  }
-  
-  free(tmp);
-  list->length--;
-  
-  if(list->length == 0)
-  {
-    list->head = NULL;
-    list->tail = NULL;
-  }
-
-  return JCRL_ERR_OK;
+    
+    if(pos > list->length)
+    {
+        return JCRL_ERR_OUT_OF_BOUNDS;
+    }
+    
+    unsigned int i = 0;
+    struct _DNode* tmp = NULL;
+    struct _DNode* ptr = list->head;
+    
+    if(pos == 0) // removing head
+    {
+        tmp = list->head;
+        
+        if(list->head->next != NULL)
+        {
+            list->head->next->prev = NULL;
+        }
+        
+        list->head = list->head->next;
+    }
+    else
+    {
+        for(ptr=list->head;ptr!=NULL;ptr=ptr->next)
+        {
+            if(i + 1 == pos)
+            {
+                tmp = ptr->next;
+            
+                if(ptr->next->next != NULL)
+                {
+                    ptr->next->next->prev = ptr;
+                }
+                else
+                {
+                    list->tail = ptr;
+                }
+            
+                ptr->next = ptr->next->next;
+            
+                break;
+            }
+        
+            i++;
+        }
+    }
+    
+    free(tmp);
+    list->length--;
+    
+    if(list->length == 0)
+    {
+        list->head = NULL;
+        list->tail = NULL;
+    }
+    
+    return JCRL_ERR_OK;
 }
 
-unsigned int dlist_traverse(unsigned int reverse, void (*callback)(void*, unsigned int), DList* list)
+unsigned int dlist_traverse(unsigned int reverse, 
+                            void (*callback)(void*, unsigned int), 
+                            DList* list)
 {
-  if(callback == NULL || list == NULL)
-  {
-    return JCRL_ERR_NULL_PARAM;
-  }
-  
-  unsigned int i = 0;
-  struct _DNode* ptr = NULL;
-  
-  if(reverse == 0)
-  {
-    ptr = list->head;
-  
-    for(ptr=list->head;ptr!=NULL;ptr=ptr->next)
+    if(callback == NULL || list == NULL)
     {
-      callback(ptr->data, i);
-      i++;
+        return JCRL_ERR_NULL_PARAM;
     }
-  }
-  else if(reverse == 1)
-  {
-    ptr = list->tail;
     
-    for(ptr=list->tail;ptr!=NULL;ptr=ptr->prev)
+    unsigned int i = 0;
+    struct _DNode* ptr = NULL;
+    
+    if(reverse == 0)
     {
-      callback(ptr->data, i);
-      i++;
+        ptr = list->head;
+    
+        for(ptr=list->head;ptr!=NULL;ptr=ptr->next)
+        {
+        callback(ptr->data, i);
+        i++;
+        }
     }
-  }
-  
-  return JCRL_ERR_OK;
+    else if(reverse == 1)
+    {
+        ptr = list->tail;
+        
+        for(ptr=list->tail;ptr!=NULL;ptr=ptr->prev)
+        {
+        callback(ptr->data, i);
+        i++;
+        }
+    }
+    
+    return JCRL_ERR_OK;
 }
 
 unsigned int dlist_get(void** data, unsigned int pos, DList* list)
 {
-  if(data == NULL || list == NULL)
-  {
-    return JCRL_ERR_NULL_PARAM;
-  }
-  
-  if(pos >= list->length)
-  {
-    return JCRL_ERR_OUT_OF_BOUNDS;
-  }
-  
-  unsigned int i = 0;
-  struct _DNode* ptr = list->head;
-  
-  for(ptr=list->head;ptr!=NULL;ptr=ptr->next)
-  {
-    if(i == pos)
+    if(data == NULL || list == NULL)
     {
-      *data = ptr->data;
-      
-      break;
+        return JCRL_ERR_NULL_PARAM;
     }
     
-    i++;
-  }
-
-  return JCRL_ERR_OK;
+    if(pos >= list->length)
+    {
+        return JCRL_ERR_OUT_OF_BOUNDS;
+    }
+    
+    unsigned int i = 0;
+    struct _DNode* ptr = list->head;
+    
+    for(ptr=list->head;ptr!=NULL;ptr=ptr->next)
+    {
+        if(i == pos)
+        {
+        *data = ptr->data;
+        
+        break;
+        }
+        
+        i++;
+    }
+    
+    return JCRL_ERR_OK;
 }
 
 unsigned int dlist_set(void* data, unsigned int pos, DList* list)
 {
-  if(list == NULL)
-  {
-    return JCRL_ERR_OK;
-  }
-  
-  if(pos >= list->length)
-  {
-    return JCRL_ERR_OUT_OF_BOUNDS;
-  }
-  
-  unsigned int i = 0;
-  struct _DNode* ptr = list->head;
-  
-  for(ptr=list->head;ptr!=NULL;ptr=ptr->next)
-  {
-    if(i == pos)
+    if(list == NULL)
     {
-      ptr->data = data;
+        return JCRL_ERR_OK;
     }
     
-    i++;
-  }
-
-  return JCRL_ERR_OK;
+    if(pos >= list->length)
+    {
+        return JCRL_ERR_OUT_OF_BOUNDS;
+    }
+    
+    unsigned int i = 0;
+    struct _DNode* ptr = list->head;
+    
+    for(ptr=list->head;ptr!=NULL;ptr=ptr->next)
+    {
+        if(i == pos)
+        {
+        ptr->data = data;
+        }
+        
+        i++;
+    }
+    
+    return JCRL_ERR_OK;
 }
-
+    
 unsigned int dlist_length(unsigned int* length, DList* list)
 {
-  if(length == NULL || list == NULL)
-  {
-    return JCRL_ERR_NULL_PARAM;
-  }
-  
-  *length = list->length;
-  
-  return JCRL_ERR_OK;
+    if(length == NULL || list == NULL)
+    {
+        return JCRL_ERR_NULL_PARAM;
+    }
+    
+    *length = list->length;
+    
+    return JCRL_ERR_OK;
 }
 
-unsigned int dlist_concatenate(DList* a, DList* b, DList* c)
+unsigned int dlist_equal(bool* equal, DList* a, DList* b)
 {
-  if(a == NULL || b == NULL || c == NULL)
-  {
-    return JCRL_ERR_NULL_PARAM;
-  }
-  
-  unsigned int i = 0;
-  unsigned int res = 0;
-  
-  struct _DNode* ptr = a->head;
-  
-  for(ptr=a->head;ptr!=NULL;ptr=ptr->next)
-  {
-    res = dlist_insert(ptr->data, i, c);
-	
-	if(res != JCRL_ERR_OK)
-	{
-	  return res;
-	}
-	
-	i++;
-  }
-  
-  ptr = b->head;
-  
-  for(ptr=b->head;ptr!=NULL;ptr=ptr->next)
-  {
-    res = dlist_insert(ptr->data, i, c);
-	
-	if(res != JCRL_ERR_OK)
-	{
-	  return res;
-	}
-	
-	i++;
-  }
-  
-  return JCRL_ERR_OK;
+    if(equal == NULL || a == NULL || b == NULL)
+    {
+        return JCRL_ERR_NULL_PARAM;
+    }
+    
+    if(a->length != b->length)
+    {
+        *equal = false;
+        return JCRL_ERR_OK;
+    }
+    
+    struct _DNode* a_ptr = a->head;
+    struct _DNode* b_ptr = b->head;
+    
+    for(a_ptr=a->head;a_ptr!=NULL;a_ptr=a_ptr->next)
+    {
+        if(a_ptr->data != b_ptr->data)
+        {
+            *equal = false;
+            return JCRL_ERR_OK;
+        }
+        
+        b_ptr = b_ptr->next; /* step forward in b */
+    }
+    
+    *equal = true;
+    
+    return JCRL_ERR_OK;
+}
+
+unsigned int dlist_append(void* data, DList* list)
+{
+    if(data == NULL || list == NULL)
+    {
+        return JCRL_ERR_NULL_PARAM;
+    }
+    
+    unsigned int res = dlist_insert(data, list->length, list);
+    
+    return res;
+    
+}
+
+unsigned int dlist_concatenate(DList* a, DList* b)
+{
+    if(a == NULL || b == NULL)
+    {
+        return JCRL_ERR_NULL_PARAM;
+    }
+    
+    unsigned int res = 0;
+    struct _DNode* ptr = b->head;
+    
+    for(ptr=b->head;ptr!=NULL;ptr=ptr->next)
+    {
+        res = dlist_append(ptr->data, a);
+        
+        if(res != JCRL_ERR_OK)
+        {
+            return res;
+        }
+    }
+    
+    return JCRL_ERR_OK;
 }
--- src/dlist.h
+++ src/dlist.h
@@ -1,6 +1,8 @@
 #ifndef DLIST_H_
 #define DLIST_H_
 
+#include <stdbool.h>
+
 struct _DNode
 {
   void* data;
@@ -17,22 +19,27 @@ struct _DList
 
 typedef struct _DList DList;
 
-// Initialisation
+/* Initialisation */
 unsigned int dlist_init(DList* list);
 unsigned int dlist_free(DList* list);
 
-// Operations
+/* Equality */
+unsigned int dlist_equal(bool* equal, DList* a, DList* b);
+
+/* Operations */
 unsigned int dlist_insert(void* data, unsigned int pos, DList* list);
 unsigned int dlist_remove(unsigned int pos, DList* list);
-unsigned int dlist_traverse(unsigned int reverse, void (*callback)(void*, unsigned int), DList* list);
+unsigned int dlist_traverse(unsigned int reverse,
+                            void (*callback)(void*, unsigned int),
+                            DList* list);
 
-// Access
+/* Access */
 unsigned int dlist_get(void** data, unsigned int pos, DList* list);
 unsigned int dlist_set(void* data, unsigned int pos, DList* list);
-
 unsigned int dlist_length(unsigned int* length, DList* list);
 
-// Utilities
-unsigned int dlist_concatenate(DList* a, DList* b, DList* c);
+/* Utilities */
+unsigned int dlist_append(void* data, DList* list);
+unsigned int dlist_concatenate(DList* a, DList* b);
 
 #endif // DLIST_H_
