1945e909d13e082e3dc8e78211b3d5a6696cf565
Jack McPherson <jmcph4@users.noreply.github.com>
2016-12-01 11:54:08 +1000

    Reformat queue implementation
    This commit simply reformats the queue implementation (`queue`) to
    better comply with more recent style guidelines.

--- src/queue.c
+++ src/queue.c
@@ -4,131 +4,134 @@
 #include "dlist.h"
 #include "queue.h"
 
+/* Initialisation */
 unsigned int queue_init(Queue* queue)
 {
-  if(queue == NULL)
-  {
-    return JCRL_ERR_NULL_PARAM;
-  }
+    if(queue == NULL)
+    {
+        return JCRL_ERR_NULL_PARAM;
+    }
 
-  unsigned int res = 0;
+    unsigned int res = 0;
   
-  queue->length = 0;
-  queue->list = calloc(1, sizeof(DList));
+    queue->length = 0;
+    queue->list = calloc(1, sizeof(DList));
   
-  if(queue->list == NULL)
-  {
-    return JCRL_ERR_SYS_MEM_ALLOC;
-  }
+    if(queue->list == NULL)
+    {
+        return JCRL_ERR_SYS_MEM_ALLOC;
+    }
   
-  res = dlist_init(queue->list);
-  
-  if(res != JCRL_ERR_OK)
-  {
-    return res;
-  }
-
-  return JCRL_ERR_OK;
+    res = dlist_init(queue->list);
+    
+    if(res != JCRL_ERR_OK)
+    {
+        return res;
+    }
+    
+    return JCRL_ERR_OK;
 }
 
 unsigned int queue_free(Queue* queue)
 {
-  if(queue == NULL)
-  {
-    return JCRL_ERR_NULL_PARAM;
-  }
-  
-  unsigned int res = 0;
-  
-  queue->length = 0;
-  
-  res = dlist_free(queue->list);
-  
-  if(res != JCRL_ERR_OK)
-  {
-    return res;
-  }
-  
-  return JCRL_ERR_OK;
+    if(queue == NULL)
+    {
+        return JCRL_ERR_NULL_PARAM;
+    }
+    
+    unsigned int res = 0;
+    
+    queue->length = 0;
+    
+    res = dlist_free(queue->list);
+    
+    if(res != JCRL_ERR_OK)
+    {
+        return res;
+    }
+    
+    return JCRL_ERR_OK;
 }
 
+/* Access */
+unsigned int queue_length(unsigned int* length, Queue* queue)
+{
+    if(length == NULL || queue == NULL)
+    {
+        return JCRL_ERR_NULL_PARAM;
+    }
+    
+    *length = queue->length;
+    
+    return JCRL_ERR_OK;
+}
+
+/* Operations */
 unsigned int queue_push(void* data, Queue* queue)
 {
-  if(queue == NULL)
-  {
-    return JCRL_ERR_NULL_PARAM;
-  }
-  
-  unsigned int res = 0;
-  
-  res = dlist_insert(data, 0, queue->list);
-  
-  if(res != JCRL_ERR_OK)
-  {
-    return res;
-  }
-  
-  queue->length++;
-  
-  return JCRL_ERR_OK;
+    if(queue == NULL)
+    {
+        return JCRL_ERR_NULL_PARAM;
+    }
+    
+    unsigned int res = 0;
+    
+    res = dlist_insert(data, 0, queue->list);
+    
+    if(res != JCRL_ERR_OK)
+    {
+        return res;
+    }
+    
+    queue->length++;
+    
+    return JCRL_ERR_OK;
 }
 
 unsigned int queue_pop(void** data, Queue* queue)
 {
-  if(data == NULL || queue == NULL)
-  {
-    return JCRL_ERR_NULL_PARAM;
-  }
-  
-  if(queue->length == 0)
-  {
-    return JCRL_ERR_OUT_OF_BOUNDS;
-  }
-  
-  unsigned int res = 0;
-  unsigned int* len = calloc(1, sizeof(int));
-  
-  if(len == NULL)
-  {
-    return JCRL_ERR_SYS_MEM_ALLOC;
-  }
-  
-  res = dlist_length(len, queue->list);
-  
-  if(res != JCRL_ERR_OK)
-  {
-    return res;
-  }
-  
-  res = dlist_get(data, *len-1, queue->list);
-  
-  if(res != JCRL_ERR_OK)
-  {
-    return res;
-  }
-  
-  res = dlist_remove(*len-1, queue->list);
-  
-  if(res != JCRL_ERR_OK)
-  {
-    return res;
-  }
-  
-  queue->length--;
-  
-  free(len);
-  
-  return JCRL_ERR_OK;
-}
-
-unsigned int queue_length(unsigned int* length, Queue* queue)
-{
-  if(length == NULL || queue == NULL)
-  {
-    return JCRL_ERR_NULL_PARAM;
-  }
-  
-  *length = queue->length;
-  
-  return JCRL_ERR_OK;
+    if(data == NULL || queue == NULL)
+    {
+        return JCRL_ERR_NULL_PARAM;
+    }
+    
+    if(queue->length == 0)
+    {
+        return JCRL_ERR_OUT_OF_BOUNDS;
+    }
+    
+    unsigned int res = 0;
+    unsigned int* len = calloc(1, sizeof(int));
+    
+    if(len == NULL)
+    {
+        return JCRL_ERR_SYS_MEM_ALLOC;
+    }
+    
+    res = dlist_length(len, queue->list);
+    
+    if(res != JCRL_ERR_OK)
+    {
+        return res;
+    }
+    
+    res = dlist_get(data, *len-1, queue->list);
+    
+    if(res != JCRL_ERR_OK)
+    {
+        return res;
+    }
+    
+    res = dlist_remove(*len-1, queue->list);
+    
+    if(res != JCRL_ERR_OK)
+    {
+        return res;
+    }
+    
+    queue->length--;
+    
+    free(len);
+    
+    return JCRL_ERR_OK;
 }
--- src/queue.h
+++ src/queue.h
@@ -1,24 +1,30 @@
 #ifndef QUEUE_H_
 #define QUEUE_H_
 
+#include <stdbool.h>
+
 #include "dlist.h"
 
 struct _Queue
 {
-  unsigned int length;
-  DList* list;
+    unsigned int length;
+    DList* list;
 };
 
 typedef struct _Queue Queue;
 
-// Initialisation
+/* Initialisation */
 unsigned int queue_init(Queue* queue);
 unsigned int queue_free(Queue* queue);
 
-// Operations
+/* Equality */
+unsigned int queue_equal(bool* equal, Queue* a, Queue* b);
+
+/* Access */
+unsigned int queue_length(unsigned int* length, Queue* queue);
+
+/* Operations */
 unsigned int queue_push(void* data, Queue* queue);
 unsigned int queue_pop(void** data, Queue* queue);
-
-unsigned int queue_length(unsigned int* length, Queue* queue);
 
 #endif // QUEUE_H_
