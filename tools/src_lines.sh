4b9a1edf3120dba78f9baad4f65ed2dc53f63729
Jack McPherson <jmcph4@users.noreply.github.com>
2016-12-21 08:31:22 +1000

    Fix utility Bash scripts
    This commit fixes the two Bash scripts in `tools/`:
    
        - `src_lines.sh`
        - `test_lines.sh`
    
    New paths are now added so the scripts can access the source directory
    and unit testing directory, respectively.

--- tools/src_lines.sh
+++ tools/src_lines.sh
@@ -1,1 +1,2 @@
+cd ..
 wc -l src/* | sort -r | grep total | awk '($1=$1);1' | cut -d " " -f1 | uniq
--- tools/test_lines.sh
+++ tools/test_lines.sh
@@ -1,1 +1,2 @@
+cd ..
 wc -l tests/unit/* | sort -r | grep total | awk '($1=$1);1' | cut -d " " -f1 | uniq
