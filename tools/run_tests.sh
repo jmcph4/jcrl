14107317901a780d477ee627cef0fff6863feaaf
jmcph4 <jmcph4@users.noreply.github.com>
2017-04-09 04:30:58 +0000

    Fix testing utility script
    This commit fixes the utility script responsible for running unit tests
    (in `tools/run_tests.sh`) so that it ignores the library archive file
    produced when building the library itself (i.e. `libjcrl.a`).

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -1,8 +1,11 @@
 for test_file in build/*
 do
-    echo "============$test_file============"
-    ./"$test_file"
-    echo ""
+    if [ "$test_file" != "build/libjcrl.a" ]
+    then
+        echo "============$test_file============"
+        ./"$test_file"
+        echo ""
+    fi
 done
 
 
